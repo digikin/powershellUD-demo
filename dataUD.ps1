7d1385479ed6940d76fca9b16c04f216b2d39d4b
Eric Stumbo <ericstumbo@student.purdueglobal.edu>
2019-05-27 20:46:50 -0400

    Working on using powershell data for PSUD

---
+++ dataUD.ps1
@@ -0,0 +1,79 @@
+$Theme = New-UDTheme -Name "Azure2" -Definition @{
+    UDDashboard = @{
+        BackgroundColor = "#333333"
+        FontColor       = "#FFFFF"
+    }
+    UDNavBar    = @{
+        BackgroundColor = "#1c1c1c"
+        FontColor       = "#55b3ff"
+    }
+    UDCard      = @{
+        BackgroundColor = "#252525"
+        FontColor       = "#FFFFFF"
+    }
+    UDChart     = @{
+        BackgroundColor = "#252525"
+        FontColor       = "#FFFFFF"
+    }
+    UDMonitor   = @{
+        BackgroundColor = "#252525"
+        FontColor       = "#FFFFFF"
+    }
+    UDTable     = @{
+        BackgroundColor = "#252525"
+        FontColor       = "#FFFFFF"
+    }
+    UDGrid      = @{
+        BackgroundColor = "#252525"
+        FontColor       = "#FFFFFF"
+    }
+    UDCounter   = @{
+        BackgroundColor = "#252525"
+        FontColor       = "#FFFFFF"
+    }
+    UDInput     = @{
+        BackgroundColor = "#252525"
+        FontColor       = "#FFFFFF"
+    }
+    UDFooter    = @{
+        BackgroundColor = "#1c1c1c"
+        FontColor       = "#55b3ff"
+    }
+}
+
+$Footer = New-UDFooter -Copyright 'Eric Stumbo' -Links $Link
+$Link = New-UDLink -text 'GitHub' -Url 'https://github.com/digikin/'
+
+
+$dataUD = New-UDDashboard -Title "ITPro Camp Demo" -Theme $Theme -Footer $Footer -Content {
+    New-UDCard -Fontcolor 'black' -Title "Hello, ITPro camp!" 
+    New-UDRow -Columns {
+        New-UDColumn -Size 3 -Content { }
+        New-UDColumn -Size 6 -Content {              
+            New-UDMonitor -Title "Processor (% Time)" -Type Line -DataPointHistory 100 -RefreshInterval 5 -Endpoint {
+                Get-Counter "\Processor(*)\% Processor Time" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-UDMonitorData
+            }
+        }
+    }
+    New-UDRow -Columns {
+        New-UDColumn -Size 3 -Content { }
+        New-UDColumn -Size 6 -Content {              
+            New-UDMonitor -Title "Network (IO Read Bytes/sec)" -Type Line -DataPointHistory 100 -RefreshInterval 1 -Endpoint {
+                ((Get-Counter "\network adapter(killer e2500 gigabit ethernet controller _4)\packets sent/sec").countersamples).CookedValue | Out-UDMonitorData
+            }
+        }
+    }
+    New-UDRow -Columns {
+        New-UDColumn -Size 3 -Content { }
+        New-UDColumn -Size 6 -Content {   
+            New-UDChart -Title "Threads by Process" -Type Doughnut -RefreshInterval 5 -Endpoint {  
+                Get-Process | ForEach-Object { [PSCustomObject]@{ Name = $_.Name; Threads = $_.Threads.Count } } | Out-UDChartData -DataProperty "Threads" -LabelProperty "Name"  
+            } -Options @{  
+                legend = @{  
+                    display = $false  
+                }  
+            }
+        }
+    }
+}
+Start-UDDashboard -Port 1001 -Dashboard $dataUD -Name 'Demo' -AutoReload
---
+++ perf_demo.ps1
@@ -0,0 +1,40 @@
+Get-Help Counter
+Get-Help Counter -examples
+
+$cpu = (Get-Counter -ListSet *).Paths
+$cpu
+
+$p = Get-Counter -Counter "\Processor(_Total)\% Processor Time"
+$q = Get-Counter -Counter "\Process(idle)\% processor time"
+$r = Get-Counter -Counter "\logicaldisk(_total)\% free space"
+$p.CounterSamples
+$q.CounterSamples
+$r.CounterSamples
+
+$z = Get-Counter -Counter "\Memory\Available MBytes"
+$z
+
+$Counters = @(
+    '\network adapter(hyper-v virtual ethernet adapter _2)\packets sent/sec'
+    '\network adapter(hyper-v virtual ethernet adapter _2)\packets received/sec'
+    '\network adapter(killer e2500 gigabit ethernet controller _3)\packets sent/sec'
+    '\network adapter(killer e2500 gigabit ethernet controller _3)\packets received/sec'
+    '\Memory\Available MBytes'
+    '\Memory\% Committed Bytes In Use'
+    '\logicaldisk(c:)\free megabytes'
+    '\logicaldisk(d:)\free megabytes'
+    '\Processor(*)\% Processor Time'
+    '\process(idle)\% processor time'
+    '\process(system)\% processor time'
+    '\processor(_total)\% idle time'
+)
+
+Get-Counter -Counter $Counters -MaxSamples 5 | ForEach {
+    $_.CounterSamples | ForEach {
+        [pscustomobject]@{
+            TimeStamp = $_.TimeStamp
+            Path = $_.Path
+            Value = $_.CookedValue
+        }
+    }
+} | Export-Csv -Path Perf.csv -NoTypeInformation
