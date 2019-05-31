c2bc9e3767cf4ebb8a2b70907c98a623b3f55133
Eric Stumbo <ericstumbo@student.purdueglobal.edu>
2019-05-30 21:39:59 -0400

    Tweaking the dashboard

--- dataUD.ps1
+++ dataUD.ps1
@@ -1,79 +1,41 @@
-$Theme = New-UDTheme -Name "Azure2" -Definition @{
-    UDDashboard = @{
-        BackgroundColor = "#333333"
-        FontColor       = "#FFFFF"
-    }
-    UDNavBar    = @{
-        BackgroundColor = "#1c1c1c"
-        FontColor       = "#55b3ff"
-    }
-    UDCard      = @{
-        BackgroundColor = "#252525"
-        FontColor       = "#FFFFFF"
-    }
-    UDChart     = @{
-        BackgroundColor = "#252525"
-        FontColor       = "#FFFFFF"
-    }
-    UDMonitor   = @{
-        BackgroundColor = "#252525"
-        FontColor       = "#FFFFFF"
-    }
-    UDTable     = @{
-        BackgroundColor = "#252525"
-        FontColor       = "#FFFFFF"
-    }
-    UDGrid      = @{
-        BackgroundColor = "#252525"
-        FontColor       = "#FFFFFF"
-    }
-    UDCounter   = @{
-        BackgroundColor = "#252525"
-        FontColor       = "#FFFFFF"
-    }
-    UDInput     = @{
-        BackgroundColor = "#252525"
-        FontColor       = "#FFFFFF"
-    }
-    UDFooter    = @{
-        BackgroundColor = "#1c1c1c"
-        FontColor       = "#55b3ff"
-    }
-}
+$Theme = Get-UDTheme Azure
 
 $Footer = New-UDFooter -Copyright 'I dont care copy the crap out of this: by Eric Stumbo' -Links $Link
 $Link = New-UDLink -text 'GitHub' -Url 'https://github.com/digikin/'
+##I tried to set up a scheduler because no matter what I tried I cant get this dashboard to pull data.  SO I am trying to Cache the info.
+$Schedule = New-UDEndpointSchedule -Every 1 -Second
+$Endpoint = New-UDEndpoint -Schedule $Schedule -Endpoint {
+    $Cache:Computers = Get-Counter '\Processor(_Total)\% Processor Time' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue
+}
 
 
 $dataUD = New-UDDashboard -Title "ITPro Camp Demo" -Theme $Theme -Footer $Footer -Content {
     New-UDCard -Fontcolor 'black' -Title "Hello, ITPro camp!" 
-    New-UDRow -Columns {
-        New-UDColumn -Size 3 -Content { }
-        New-UDColumn -Size 6 -Content {              
-            New-UDMonitor -Title "Processor (% Time)" -Type Line -DataPointHistory 100 -RefreshInterval 5 -Endpoint {
-                Get-Counter "\Processor(*)\% Processor Time" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-UDMonitorData
+    New-UDRow {
+        New-UDColumn -LargeSize 2 -Content { }
+        New-UDColumn -LargeSize 4 -Content {
+            New-UDChart -Title "Localhost - CPU (% processor time)" -Type Line -AutoRefresh -RefreshInterval 5 -Endpoint {
+                $Cache:Computers | Out-UDChartData -DataProperty "MB" -LabelProperty "Date"
             }
-        }
-    }
-    New-UDRow -Columns {
-        New-UDColumn -Size 3 -Content { }
-        New-UDColumn -Size 6 -Content {              
-            New-UDMonitor -Title "Network (IO Read Bytes/sec)" -Type Line -DataPointHistory 100 -RefreshInterval 1 -Endpoint {
-                ((Get-Counter "\network adapter(killer e2500 gigabit ethernet controller _4)\packets sent/sec").countersamples).CookedValue | Out-UDMonitorData
+            New-UdMonitor -Title "$ServerOne - CPU (% processor time)" -Type Line -DataPointHistory 20 -RefreshInterval 5 -ChartBackgroundColor '#80FF6B63' -ChartBorderColor '#FFFF6B63'  -Endpoint {
+                Get-Counter -ComputerName $ServerOne '\Processor(_Total)\% Processor Time' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-UDMonitorData
             }
+
         }
-    }
-    New-UDRow -Columns {
-        New-UDColumn -Size 3 -Content { }
-        New-UDColumn -Size 6 -Content {   
+        New-UDColumn -LargeSize 4 -Content {
+            New-UdMonitor -Title "LocalHost - Memory (Available MB)" -Type Doughnut -ChartBackgroundColor '#80FF6B63' -ChartBorderColor '#FFFF6B63'  -Endpoint {
+                Get-Counter '\logicaldisk(_total)\% free space' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue | Out-UDMonitorData
+            }
             New-UDChart -Title "Threads by Process" -Type Doughnut -RefreshInterval 5 -Endpoint {  
                 Get-Process | ForEach-Object { [PSCustomObject]@{ Name = $_.Name; Threads = $_.Threads.Count } } | Out-UDChartData -DataProperty "Threads" -LabelProperty "Name"  
             } -Options @{  
                 legend = @{  
                     display = $false  
                 }  
-            }
+            }           
         }
+        New-UDColumn -LargeSize 2 -Content { }
     }
+
 }
-Start-UDDashboard -Port 1001 -Dashboard $dataUD -Name 'Demo' -AutoReload
+Start-UDDashboard -Port 10001 -Dashboard $dataUD -EndPoint $EndPoint -Name 'Demo' 
